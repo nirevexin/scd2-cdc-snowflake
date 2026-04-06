67c3a959672efc4b49d0574969a8ddaa69bda4fe
nirevexin <137289132+nirevexin@users.noreply.github.com>
2026-04-06 14:58:35 +0200

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,14 +1,12 @@
-# Setup
-
-```sql
 -- Run once
+```sql
 CREATE TABLE IF NOT EXISTS cdc_processing (
     last_processed_cdc_log_position INT DEFAULT 0
 );
 ```
 
-```sql
 -- Insert initial value if table was empty
+```sql
 INSERT INTO cdc_processing (last_processed_cdc_log_position)
 SELECT 0
 WHERE NOT EXISTS (SELECT 1 FROM cdc_processing);
